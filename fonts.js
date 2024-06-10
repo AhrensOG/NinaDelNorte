2e2e98284363e53916b81521563695ef1725e518
AhrensOG <ahrensgabriel09@gmail.com>
2024-06-10 18:18:28 -0300

    Add fonts | Add principal colours | Add Logos and other images

---
+++ fonts.js
@@ -0,0 +1,16 @@
+import { Roboto } from "next/font/google";
+import { Great_Vibes } from "next/font/google";
+import { Alegreya } from "next/font/google";
+
+export const RobotoFont = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+export const GreatVibesFont = Great_Vibes({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+export const AlegreyaFont = Alegreya({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
